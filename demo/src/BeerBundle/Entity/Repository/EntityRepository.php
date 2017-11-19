323d13f002221d73eb8ede195b06daaf5ae695d4
romain <romain@akeneo.com>
2017-11-19 23:20:24 +0100

    Rework entity repository

--- demo/src/BeerBundle/Entity/Repository/EntityRepository.php
+++ demo/src/BeerBundle/Entity/Repository/EntityRepository.php
@@ -13,24 +13,6 @@ class EntityRepository extends BaseEntityRepository implements IdentifiableRepos
 {
     public function findOneByIdentifier(string $code)
     {
-        $qb = $this->createQueryBuilder('c');
-        $qb->andWhere(
-            $qb->expr()->like('c.code', ':code')
-        );
-        $qb->setParameter('code', $code);
-
-        $results = $qb->getQuery()->execute();
-
-        $count = count($results);
-
-        if ($count === 0) {
-            return null;
-        }
-
-        if ($count > 1) {
-            throw new \Exception('This query should return only one element');
-        }
-
-        return $results[0];
+        return $this->findOneBy(['code' => $code]);
     }
 }
