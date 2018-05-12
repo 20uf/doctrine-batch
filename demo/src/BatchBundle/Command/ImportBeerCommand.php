d9b0a8f97bdcd382633d1d8d5a0103b36e877e38
romain <romain@akeneo.com>
2018-05-12 14:38:36 +0200

    Rework import beer

--- demo/src/BatchBundle/Command/ImportBeerCommand.php
+++ demo/src/BatchBundle/Command/ImportBeerCommand.php
@@ -6,13 +6,16 @@ use BatchBundle\Job\Job;
 use BatchBundle\Processor\BeerProcessor;
 use BatchBundle\Reader\CsvReader;
 use BatchBundle\Writer\EntityWriter;
+use BeerBundle\Entity\Beer;
 use BeerBundle\Entity\Repository\IdentifiableRepositoryInterface;
 use BeerBundle\Utils\CommandLogger;
+use BeerBundle\Utils\Timer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
@@ -39,18 +42,74 @@ class ImportBeerCommand extends ContainerAwareCommand
             throw new \Exception(sprintf('File "%s" not found', $filepath));
         }
 
-        $reader = new CsvReader($filepath);
-        $processor = new BeerProcessor(
-            $this->getBeerRepository(),
-            $this->getBreweryRepository(),
-            $this->getCategoryRepository()
-        );
-        $writer = new EntityWriter($this->getEntityManager(), $this->getValidator());
+        CommandLogger::memory('Import');
+        Timer::startTime('Import');
 
-        $job = new Job($reader, $processor, $writer);
-        $job->setBatchSize(100);
+        // Read content
+        $writeCount = 0;
+        $fd = fopen($filepath, 'r+');
+        $headers = fgetcsv($fd, null, ';');
 
-        $job->execute();
+        while ($csvRow = fgetcsv($fd, null, ';')) {
+            $csvRow = array_combine($headers, $csvRow);
+
+            // Process data row
+            $entity = $this->process($csvRow);
+
+            // Validate entity
+            $violations = $this->getValidator()->validate($entity);
+            if ($violations->count() === 0) {
+                // Write entity
+                $this->getEntityManager()->persist($entity);
+                if (0 === ++$writeCount % 100) {
+                    $this->getEntityManager()->flush();
+                }
+            } else {
+                $this->printViolations($violations, $entity);
+            }
+        }
+        CommandLogger::info(sprintf('%s entity written', $writeCount));
+
+        fclose($fd);
+        CommandLogger::timeAndMemory('Import');
+    }
+
+    private function process(array $item)
+    {
+        $beer = $this->findOrCreateBeer($item['code']);
+        $beer->setName($item['name']);
+        $beer->setDescription($item['description']);
+        $beer->setPercent($item['percent']);
+        $beer->setQuotation($item['quotation']);
+
+        $category = $this->getCategoryRepository()->findOneByIdentifier($item['category']);
+        $beer->setCategory($category);
+
+        $brewery = $this->getBreweryRepository()->findOneByIdentifier($item['brewery']);
+        $beer->setBrewery($brewery);
+
+        return $beer;
+    }
+
+    private function findOrCreateBeer(string $code)
+    {
+        $entity = $this->getBeerRepository()->findOneByIdentifier($code);
+        if (null === $entity) {
+            $entity = new Beer();
+            $entity->setCode($code);
+
+        }
+
+        return $entity;
+    }
+
+    private function printViolations(ConstraintViolationList $violations, $item)
+    {
+        foreach ($violations as $violation) {
+            CommandLogger::error(
+                sprintf('Entity "%s" not valid: %s', $item->getCode(), $violation->getMessage())
+            );
+        }
     }
 
     /**
